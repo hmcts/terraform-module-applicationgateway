4db895a2c1ac42fa7e42306a8fc02e3d8cafb9b3
Enda Kelly <enda.kelly@hmcts.net>
2022-06-22 15:45:19 +0100

    add port to probe

--- main.tf
+++ main.tf
@@ -77,6 +77,7 @@ resource "azurerm_application_gateway" "ag" {
       name                = probe.value.name
       host                = probe.value.host
       path                = probe.value.path
+      port                = 0
       protocol            = "Http"
       timeout             = 15
       unhealthy_threshold = 3
