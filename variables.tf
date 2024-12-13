1fdf8ee602207b54b09095f15625d29a60cbb33a
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2024-12-13 14:48:04 +0000

    editing naming convention

--- variables.tf
+++ variables.tf
@@ -90,3 +90,9 @@ variable "waf_mode" {
 variable "exclusions" {
   default = []
 }
+
+variable "app_gateway_name" {
+  description = "The name of the Application Gateway"
+  type        = string
+  default     = null
+}
